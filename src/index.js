97caf04e0f4e4aa8e66341fa391a99b6198a9675
Muhammad Annas Khan <annasking601@gmail.com>
2026-01-30 15:50:38 +0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import { app } from "./app.js";
+// import { app } from "./app.js";
+import app from "./app.js"
 
 dotenv.config();
 
